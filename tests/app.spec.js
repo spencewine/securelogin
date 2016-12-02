68c55dd10dbd0203b75bfe2b5cc50e067bd18cd8
dejongnj <dejongnj@gmail.com>
2016-12-02 09:14:55 -0600

    set up testing encironment for server with one nonsense test

---
+++ tests/app.spec.js
@@ -0,0 +1,9 @@
+/* global describe it xit expect */
+
+const { expect } = require('chai');
+
+describe('app.js in root folder', () => {
+  it('', () => {
+    expect(true).to.be.equal(true);
+  });
+});
