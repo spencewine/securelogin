12af7f44bf7b468335654eed5428e0c9fcf1c8d6
dejongnj <dejongnj@gmail.com>
2016-12-05 08:31:14 -0600

    added react router

--- src/client/jsx/index.jsx
+++ src/client/jsx/index.jsx
@@ -1,7 +1,14 @@
 /* global document */
 import React from 'react';
 import { render } from 'react-dom';
+import { Router, Route, browserHistory } from 'react-router';
 import style from '../assets/stylesheets/main.scss'; // eslint-disable-line
 import App from './components/App';
 
-render(<App />, document.getElementById('app'));
+const rootRouter = (
+  <Router history={browserHistory} >
+    <Route path="/" component={App} />
+  </Router>
+);
+
+render(rootRouter, document.getElementById('app'));
